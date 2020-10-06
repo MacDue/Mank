88161d252039726890359180ffc96d4aaf9586b7
MacDue <macdue@dueutil.tech>
2020-10-06 13:33:45 +0100

    [Tests] add support for more expressions/statements in the AST builder

--- tests/helpers/ast_builder.h
+++ tests/helpers/ast_builder.h
@@ -4,7 +4,19 @@
 
 #include "ast.h"
 
-/* Constructs */
+template <typename TStmt>
+Statement_Ptr to_stmt_ptr(TStmt && stmt) {
+  stmt.location = {};
+  return std::make_shared<Ast_Statement>(stmt);
+}
+
+template <typename TExpr>
+Expression_Ptr to_expr_ptr(TExpr && expr) {
+  expr.location = {};
+  return std::make_shared<Ast_Expression>(expr);
+}
+
+/* -- Constructs -- */
 
 template<typename... TFunction>
 Ast_File make_file(TFunction && ... function) {
@@ -14,6 +26,8 @@ Ast_File make_file(TFunction && ... function) {
     std::make_shared<Type>(function)...);
   return file;
 }
+
+/* Args */
 
 inline Ast_Argument make_argument(Type_Ptr type, std::string name) {
   return Ast_Argument {
@@ -26,6 +40,8 @@ template<typename... TArgs>
 std::vector<Ast_Argument> make_args(TArgs && ... args) {
   return std::vector<Ast_Argument>{ args... };
 }
+
+/* Functions */
 
 inline Ast_Function_Declaration make_function(
   bool procedure,
@@ -75,6 +91,8 @@ inline Ast_Function_Declaration make_procedure(
   return make_procedure(name, {}, std::move(body));
 }
 
+/* Blocks */
+
 template <typename... TStmt>
 Ast_Block make_body(TStmt && ... stmts) {
   Ast_Block block;
@@ -82,19 +100,14 @@ Ast_Block make_body(TStmt && ... stmts) {
   return block;
 }
 
-template <typename TStmt>
-Statement_Ptr to_stmt_ptr(TStmt && stmt) {
-  stmt.location = {};
-  return std::make_shared<Ast_Statement>(stmt);
-}
-
 template <typename... TStmt>
 Statement_Ptr make_block(TStmt && ... stmts) {
   return to_stmt_ptr(make_body(stmts...));
 }
 
+/* -- Statements -- */
 
-/* Statements */
+/* Expression statement */
 
 inline Statement_Ptr make_expr_stmt(Expression_Ptr expr) {
   Ast_Expression_Statement expr_stmt;
@@ -102,15 +115,32 @@ inline Statement_Ptr make_expr_stmt(Expression_Ptr expr) {
   return to_stmt_ptr(expr_stmt);
 }
 
-// TODO
+/* Return statement */
 
-/* Expressions */
-
-template <typename TExpr>
-Expression_Ptr to_expr_ptr(TExpr && expr) {
-  expr.location = {};
-  return std::make_shared<Ast_Expression>(expr);
+inline Statement_Ptr make_return(Expression_Ptr value) {
+  Ast_Return_Statement return_stmt;
+  return_stmt.expression = value;
+  return to_stmt_ptr(return_stmt);
 }
+
+/* If statement */
+
+inline Statement_Ptr make_if(
+  Expression_Ptr cond, Statement_Ptr then_block, Statement_Ptr else_block = nullptr
+) {
+  Ast_If_Statement if_stmt;
+  if_stmt.cond = cond;
+  if_stmt.then_block = then_block;
+  if (else_block) {
+    if_stmt.has_else = true;
+    if_stmt.else_block = else_block;
+  }
+  return to_stmt_ptr(if_stmt);
+}
+
+/* -- Expressions -- */
+
+/* Literals */
 
 inline Expression_Ptr make_literal(PrimativeTypeTag type, std::string value) {
   Ast_Literal literal;
@@ -123,11 +153,23 @@ inline Expression_Ptr make_string(std::string value) {
   return make_literal(PrimativeTypeTag::STRING, value);
 }
 
+inline Expression_Ptr make_integer(int value) {
+  return make_literal(PrimativeTypeTag::INTEGER, std::to_string(value));
+}
+
+inline Expression_Ptr make_float64(double value) {
+  return make_literal(PrimativeTypeTag::FLOAT64, std::to_string(value));
+}
+
+/* Idents */
+
 inline Expression_Ptr make_ident(std::string name) {
   Ast_Identifier ident;
   ident.name = name;
   return to_expr_ptr(ident);
 }
+
+/* Calls */
 
 template <typename... TArgs>
 Expression_Ptr make_call(Expression_Ptr callee, TArgs && ... args) {
@@ -142,4 +184,23 @@ Expression_Ptr make_call(std::string callee, TArgs && ... args) {
   return make_call(make_ident(callee), args...);
 }
 
-// TODO
+/* Unary */
+
+inline Expression_Ptr make_unary(Ast_Operator op, Expression_Ptr operand) {
+  Ast_Unary_Operation unary;
+  unary.operation = op;
+  unary.operand = operand;
+  return to_expr_ptr(unary);
+}
+
+/* Binary */
+
+inline Expression_Ptr make_binary(
+  Ast_Operator op, Expression_Ptr left, Expression_Ptr right
+) {
+  Ast_Binary_Operation binop;
+  binop.operation = op;
+  binop.left = left;
+  binop.right = right;
+  return to_expr_ptr(binop);
+}

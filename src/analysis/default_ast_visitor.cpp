1d58f3ab5a9cb78eff7a81a5bd5fb91f295aa2b8
MacDue <macdue@dueutil.tech>
2020-10-06 18:54:30 +0100

    [Util] improve AstVisitor api

--- src/analysis/default_ast_visitor.cpp
+++ src/analysis/default_ast_visitor.cpp
@@ -6,65 +6,93 @@
 
 /* Constructs */
 
-void AstVisitor::operator()(Ast_File& file) {
+void BaseAstVisitor::operator()(Ast_File& file) {
+  before(file);
+  visit(file);
   for (auto& func_type: file.functions) {
     auto& func = std::get<Ast_Function_Declaration>(func_type->v);
     recur(func);
   }
+  after(file);
 }
 
-void AstVisitor::operator()(Ast_Function_Declaration& func) {
+void BaseAstVisitor::operator()(Ast_Function_Declaration& func) {
+  before(func);
+  visit(func);
   recur(func.body);
+  after(func);
 }
 
 /* Statements */
 
-void AstVisitor::operator()(Ast_Expression_Statement& expr_stmt) {
+void BaseAstVisitor::operator()(Ast_Expression_Statement& expr_stmt) {
+  before(expr_stmt);
+  visit(expr_stmt);
   std::visit(recur, expr_stmt.expression->v);
+  after(expr_stmt);
 }
 
-void AstVisitor::operator()(Ast_Return_Statement& return_stmt) {
+void BaseAstVisitor::operator()(Ast_Return_Statement& return_stmt) {
+  before(return_stmt);
+  visit(return_stmt);
   std::visit(recur, return_stmt.expression->v);
 }
 
-void AstVisitor::operator()(Ast_Block& block) {
+void BaseAstVisitor::operator()(Ast_Block& block) {
+  before(block);
+  visit(block);
   for (auto& stmt: block.statements) {
     std::visit(recur, stmt->v);
   }
+  after(block);
 }
 
-void AstVisitor::operator()(Ast_If_Statement& if_stmt) {
+void BaseAstVisitor::operator()(Ast_If_Statement& if_stmt) {
+  before(if_stmt);
+  visit(if_stmt);
   std::visit(recur, if_stmt.cond->v);
   std::visit(recur, if_stmt.then_block->v);
   if (if_stmt.has_else) {
     std::visit(recur, if_stmt.else_block->v);
   }
+  after(if_stmt);
 }
 
 /* Expressions */
 
-void AstVisitor::operator()(Ast_Call& call) {
+void BaseAstVisitor::operator()(Ast_Call& call) {
+  before(call);
+  visit(call);
   std::visit(recur, call.callee->v);
   for (auto& arg: call.arguments) {
     std::visit(recur, arg->v);
   }
+  after(call);
 }
 
-void AstVisitor::operator()(Ast_Literal& literal) {
-  /* that'll do pig, that'll do */
-  (void) literal;
+void BaseAstVisitor::operator()(Ast_Literal& literal) {
+  before(literal);
+  visit(literal);
+  after(literal);
 }
 
-void AstVisitor::operator()(Ast_Identifier& ident) {
-  /* we've hit rock bottom */
-  (void) ident;
+void BaseAstVisitor::operator()(Ast_Identifier& ident) {
+  before(ident);
+  visit(ident);
+  after(ident);
 }
 
-void AstVisitor::operator()(Ast_Unary_Operation& unary) {
+void BaseAstVisitor::operator()(Ast_Unary_Operation& unary) {
+  before(unary);
+  visit(unary);
   std::visit(recur, unary.operand->v);
+  after(unary);
 }
 
-void AstVisitor::operator()(Ast_Binary_Operation& binop) {
+void BaseAstVisitor::operator()(Ast_Binary_Operation& binop) {
+  before(binop);
+  visit(binop);
   std::visit(recur, binop.left->v);
   std::visit(recur, binop.right->v);
+  after(binop);
 }
--- src/analysis/include/ast/visitor.h
+++ src/analysis/include/ast/visitor.h
@@ -1,23 +1,74 @@
 #pragma once
 
+#include <type_traits>
+
 #include "ast.h"
 
-struct AstVisitor {
-  using type = void;
+/*
+  Basically all the following does is allow me not to define all
+  the before/after/visit functions for each node just the operator().
+
+  The point of AstVisitor is to wrap around all the std::visit calls,
+  and to allow you to traverse the AST without needing to do all the
+  traversal explictly.
+
+  before/after are just there for neatness
+  visit is there to hide overloading operator() -- which is a little clunky
+*/
+
+template<typename TAst, typename... TAstRest>
+struct AstVisitor;
+
+/* Base visitor */
+template<typename TAst>
+struct AstVisitor<TAst> {
+  virtual void before(TAst&) {};
+  virtual void after(TAst&)  {};
+  virtual void visit(TAst&)  {};
+
+  virtual void operator()(TAst&) = 0;
+};
+
+#define with(func)                    \
+  using AstVisitor<TAst>::func;       \
+  using AstVisitor<TAstRest...>::func;\
+
+/* recursion! */
+template<typename TAst, typename... TAstRest>
+struct AstVisitor: public AstVisitor<TAst>, public AstVisitor<TAstRest...>
+{
+  with(visit); with(before); with(after);
+};
+
+#undef with
+
+struct BaseAstVisitor: public AstVisitor<
   /* Constructs */
-  virtual void operator()(Ast_File& file);
-  virtual void operator()(Ast_Function_Declaration& func);
-
+  Ast_File,
+  Ast_Function_Declaration,
   /* Statements */
-  virtual void operator()(Ast_Expression_Statement& expr_stmt);
-  virtual void operator()(Ast_Return_Statement& return_stmt);
-  virtual void operator()(Ast_Block& ast_block);
-  virtual void operator()(Ast_If_Statement& if_stmt);
-
+  Ast_Expression_Statement,
+  Ast_Return_Statement,
+  Ast_Block,
+  Ast_If_Statement,
   /* Expressions */
-  virtual void operator()(Ast_Call& call);
-  virtual void operator()(Ast_Literal& literal);
-  virtual void operator()(Ast_Identifier& ident);
-  virtual void operator()(Ast_Unary_Operation& unary);
-  virtual void operator()(Ast_Binary_Operation& binop);
+  Ast_Call,
+  Ast_Literal,
+  Ast_Identifier,
+  Ast_Unary_Operation,
+  Ast_Binary_Operation
+> {
+  void operator()(Ast_File& file) override;
+  void operator()(Ast_Function_Declaration& func) override;
+
+  void operator()(Ast_Expression_Statement& expr_stmt) override;
+  void operator()(Ast_Return_Statement& return_stmt) override;
+  void operator()(Ast_Block& ast_block) override;
+  void operator()(Ast_If_Statement& if_stmt) override;
+
+  void operator()(Ast_Call& call) override;
+  void operator()(Ast_Literal& literal) override;
+  void operator()(Ast_Identifier& ident) override;
+  void operator()(Ast_Unary_Operation& unary) override;
+  void operator()(Ast_Binary_Operation& binop) override;
 };

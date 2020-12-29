0cd4695694d4e3e13bad0a5f508f73b51d1c3a62
MacDue <macdue@dueutil.tech>
2020-12-29 14:56:28 +0000

    [Tools] helper script to compile .mank files to executables

---
+++ tools/mank.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Helper script to compile .mank files with a mank_main() to a binary
+# Usage: ./mank.sh my_prog.mank
+# Creates ./my_prog binary
+
+if [[ -z "${MANK_HOME}" ]]; then
+  MANK_HOME=$(pwd)
+else
+  MANK_HOME="${MANK_HOME}"
+fi
+
+build_dir=$(mktemp --directory)
+output_dir=$(pwd)
+cd $build_dir
+
+cat << EOF > ./main.c
+extern void mank_main(void);
+
+int main() {
+  mank_main();
+}
+EOF
+
+gcc ./main.c -c -o main.o
+$MANK_HOME/mankc $output_dir/$1 --codegen --suppress-warnings > mank_main.ll
+llc ./mank_main.ll
+
+bin_name=$(basename $1 .mank)
+gcc ./main.o ./mank_main.s -o ./$bin_name
+
+mv ./$bin_name $output_dir/$bin_name
+

e6fa023cdf19424f537230f64d78a118c27fa2fd
MacDue <macdue@dueutil.tech>
2021-02-10 19:57:52 +0000

    [Tools] show success/fail on self tests runner

--- tools/run_self_tests.sh
+++ tools/run_self_tests.sh
@@ -9,13 +9,21 @@ MANKC="${PWD}/mank.sh"
 MANK_HOME="${PWD}/../build"
 export MANK_HOME
 
+NC='\033[0m'
+RED='\033[1;31m'
+GREEN='\033[0;32m'
+
 for tests in ${TESTS_DIR}/self_tests/*.mank
 do
   test_name=$(basename $tests .mank)
   echo "Compiling tests ${test_name}.mank"
   build_dir=$(mktemp --directory)
   cd $build_dir
-  ABS_SOURCE_PATH=1 $MANKC $tests --tests
-  echo "Running test ${test_name}.mank:"
-  ./${test_name}_tests
+  {
+    ABS_SOURCE_PATH=1 $MANKC $tests --tests \
+    && echo "Running test ${test_name}.mank:" \
+    && ./${test_name}_tests && echo -e "${GREEN}Test ${test_name} PASS!${NC}"
+  } || {
+    echo -e "${RED}Test ${test_name} FAILED!${NC}"
+  }
 done

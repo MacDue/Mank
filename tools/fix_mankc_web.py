0bee8294f572632828744076ca1bb565965b33cc
MacDue <macdue@dueutil.tech>
2021-03-15 17:10:22 +0000

    [WASM] just use locateFile to locate the module

--- tools/fix_mankc_web.py
+++ tools/fix_mankc_web.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     mank_js = f.read()
 
   # Fix a few issues to allow use with create react app
-  mank_js = mank_js.replace("mankc_web.wasm", "/mankc_web.wasm")
-  mank_js = mank_js.replace("wasmBinaryFile=locateFile(wasmBinaryFile)", "/*nop*/")
+  # mank_js = mank_js.replace("mankc_web.wasm", "/mankc_web.wasm")
+  # mank_js = mank_js.replace("wasmBinaryFile=locateFile(wasmBinaryFile)", "/*nop*/")
   mank_js = "/* eslint-disable */\n" + mank_js
 
   outpath = path.replace(".js", ".cra.js")
